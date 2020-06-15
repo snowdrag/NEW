7d170a4a8fa49a97f79f7332048e277c7bf4dc8b
Li Song <lisong@notimportant.org>
2020-06-15 23:29:12 +0100

    add about

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react"
+import ReactMarkdown from "react-markdown"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="About: Yulin Huang" />
+    <h1>About</h1>
+
+    <ReactMarkdown source={data.strapiAbout.body} />
+  </Layout>
+)
+
+export default AboutPage
+
+export const pageQuery = graphql`
+  query AboutQuery {
+    strapiAbout {
+      body
+    }
+  }
+`
